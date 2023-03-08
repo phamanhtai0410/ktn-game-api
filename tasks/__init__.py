87ba9702fc02989b336f6c9a87a16920b42aa6c5
Pham Anh Tai <phamanhtai0410@gmail.com>
2023-03-08 10:01:33 +0700

    erase task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,8 +4,6 @@
         -
         -
 """
-from .hello import task_hello
-from .model import task_model
-from .referral import task_generate_referral_code, task_calculate_referral_rank
-from .wallet import task_wallet_exchange
+
+
 
