cefe77709c06f864ee89731a582b3a20b1a3e427
Pham Anh Tai <phamanhtai0410@gmail.com>
2023-04-29 17:44:19 +0700

    [ExistingMetadataCollection] add default price

--- helper/collection.py
+++ helper/collection.py
@@ -189,6 +189,7 @@ class CollectionHelper:
                     # Category
                     "category": _category,
                     # Set default value for collection
+                    "price": _default_price,
                     "commission": float(_commision_default),
                     "commission_level_2": float(_commision_level_2_default),
                     "discount": float(_discount_default),
